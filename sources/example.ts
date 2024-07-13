6e6dd170bf51ad4388db991587aa37b381546c76
Javier R <javier.rios-urbano@epitech.eu>
2024-07-13 18:42:32 +0200

    add: tentative `API` code

--- sources/example.ts
+++ sources/example.ts
@@ -2,4 +2,34 @@ import { init } from "~/sources/mod.ts";
 
 using handle = await init();
 
-handle.hello();
+const object = {
+  username: "hello",
+  password: "world",
+  age: 20,
+  interests: {
+    programming: true,
+    languages: ["rust", "typescript", "c++"],
+  },
+  friends: [
+    {
+      name: "Alice",
+      age: 21,
+    },
+    {},
+    [12, 13, 14],
+  ],
+};
+
+const bytes = new TextEncoder().encode(JSON.stringify(object));
+const _pointer = Deno.UnsafePointer.of(bytes);
+
+const parser = handle.create_parser();
+if (parser === null) {
+  throw new Error("Failed to create parser");
+}
+
+// parser -> Blueprint::JSON::Parser*
+
+// handle->verify(parser, _pointer, $schema);
+
+handle.destroy_parser(parser);
