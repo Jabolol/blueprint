46e95c8c77d5f949b50ab805ae567c87e2300516
Javier R <javier.rios-urbano@epitech.eu>
2024-07-21 23:03:31 +0200

    fix: use `simple` example

--- sources/example.ts
+++ sources/example.ts
@@ -1,29 +1,10 @@
 import { b, type InferSchema } from "~/sources/mod.ts";
 
 using handle = await b.init();
+const schema = b.array(b.number()).min(1);
+const data: InferSchema<typeof schema> = [];
+const result = handle.verify(schema, data);
 
-const schema = b.object({
-  name: b.string().length(10),
-  age: b.number().min(18),
-  likes: b.array(b.string()).values(["dogs", "cats"]).max(5),
-  pets: b.array(b.object({
-    name: b.string().length(10),
-    age: b.number().min(0),
-    kind: b.string().enum(["dog", "cat", "bird"]),
-  })).max(3),
-});
-
-const data: InferSchema<typeof schema> = {
-  name: "Javi",
-  age: 21,
-  likes: ["cats", "birds"],
-  pets: [
-    { name: "Lupita", age: 12, kind: "dog" },
-  ],
-};
-
-console.log(schema);
-
-const result = handle.parse(schema, data);
-
-console.log(`The schema is ${result ? "valid" : "invalid"}`);
+if (!result) {
+    console.log({ error: handle.error });
+}
