e88f4c05375174d95ad3607561ad0843b89b6820
Javier R <javier.rios-urbano@epitech.eu>
2024-07-21 23:02:54 +0200

    fix: make `ffi` host owner of resources

--- library/blueprint.cpp
+++ library/blueprint.cpp
@@ -1,22 +1,39 @@
-#include "JSON/Parser.hpp"
+#include "Schema.hpp"
 
 extern "C" const char *version(void)
 {
     return "0.0.2";
 }
 
-extern "C" Blueprint::JSON::Parser *create_parser()
+extern "C" Blueprint::Schema *create(void)
 {
-    return new Blueprint::JSON::Parser();
+    return new (std::nothrow) Blueprint::Schema();
 }
 
-extern "C" void destroy_parser(Blueprint::JSON::Parser *parser)
+extern "C" void destroy(Blueprint::Schema *blueprint)
 {
-    delete parser;
+    if (blueprint == nullptr) {
+        throw std::runtime_error("'destroy' received a nullptr");
+    }
+
+    delete blueprint;
 }
 
-extern "C" bool parse(
-    Blueprint::JSON::Parser *parser, const char *schema, const char *data)
+extern "C" bool verify(
+    Blueprint::Schema *blueprint, const char *schema, const char *data)
 {
-    return true;
+    if (blueprint == nullptr) {
+        throw std::runtime_error("'verify' received a nullptr");
+    }
+
+    return blueprint->verify(schema, data);
+}
+
+extern "C" const char *error(Blueprint::Schema *blueprint)
+{
+    if (blueprint == nullptr) {
+        throw std::runtime_error("'error' received a nullptr");
+    }
+
+    return blueprint->getError().c_str();
 }
